4560482a98e943b00346f7f46015120c92957b1e
Michael Szurko <michael@MacBook-Pro-113.attlocal.net>
2024-12-23 01:21:11 -0500

    updated about me page

--- dpantom-portf/src/components/AboutPage.jsx
+++ dpantom-portf/src/components/AboutPage.jsx
@@ -7,14 +7,15 @@ function AboutPage() {
       <Card sx={{ maxWidth: 500, textAlign: 'center' }}>
         <CardContent>
           <Avatar sx={{ bgcolor: blue[500], width: 100, height: 100, margin: '0 auto 16px' }}>
-            A
+            M
           </Avatar>
           <Typography variant="h4" gutterBottom>
             About Me
           </Typography>
           <Typography variant="body1" paragraph>
-            I'm a front-end developer with a passion for creating beautiful and functional websites. I specialize in building
-            responsive and interactive web applications using modern tools like React, Material-UI, and more.
+          I’m a <strong>front-end developer</strong> passionate about creating responsive, user-friendly websites using <strong>React</strong>, <strong>Material UI</strong>, and modern web technologies. With a strong background in <strong>UX/UI design</strong> from Miami University, I focus on building seamless, intuitive experiences that prioritize <strong>both</strong> functionality and design.
+
+Currently, I’m focused on front-end development but am eager to expand into <strong>IT</strong> and <strong>cybersecurity</strong>. I’m also working to improve my <strong>Russian</strong>, which I see as a valuable asset for future global opportunities.
           </Typography>
         </CardContent>
       </Card>
