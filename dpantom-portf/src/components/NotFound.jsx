d81ffa1b89eb626cc128da3dad14331fa8e794e2
Michael Szurko <michael@MacBook-Pro-113.attlocal.net>
2024-12-25 09:17:25 -0500

    added translations for 404 error

--- dpantom-portf/src/components/NotFound.jsx
+++ dpantom-portf/src/components/NotFound.jsx
@@ -1,13 +1,16 @@
-// /components/NotFoundPage.jsx
-import 'react';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next'; 
 
 function NotFound() {
+  const { t } = useTranslation(); 
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for does not exist.</p>
-      <Link to="/">Go to Home</Link>
+      <h1>{t('notFound.title')}</h1>  {/* Translated title */}
+      <p>{t('notFound.message')}</p>  {/* Translated message */}
+      <Link to="/">{t('notFound.returnHome')}</Link>  {/* Translated button text */}
     </div>
   );
 }
